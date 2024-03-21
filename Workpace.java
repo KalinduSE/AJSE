fffccdb5bdcde65ab24483a0ee83fd815430ad8e
Kalindu <0101.Programming@gmail.com>
2024-03-21 21:35:46 +0530

    Workspace cleared after Lec. Eight (21-03-2024))

--- Workpace.java
+++ Workpace.java
@@ -3,26 +3,8 @@ import java.util.*;
 class Workpace{
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
-        int marks = input.nextInt();
+       
+    }
+    
 
-        if (marks > 74){
-            System.out.println("A"); //75-100
-        }
-        else{
-
-            if( marks > 64){
-                System.out.println("B"); //65-74
-            }
-            else{
-
-                if( marks > 54){
-                    System.out.println("C"); //64-55
-                }
-               else{
-                      System.out.println("Fail");  //54-0
-                   }
-              }
-        }
  }
-}
