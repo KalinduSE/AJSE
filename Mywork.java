1c90b1783f9402eb9575889d342268df716a65bc
Kalindu <0101.Programming@gmail.com>
2024-03-21 19:27:52 +0530

    first

---
+++ Mywork.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Mywork{
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int marks = input.nextInt();
+
+        if (marks > 74){
+            System.out.println("A");
+        }
+        else{
+
+            if( 64 < marks  && marks < 75){
+                System.out.println("B");
+            }
+            else{
+                if( 64 < marks  && marks < 75){
+                    System.out.println("B");
+                }
+                else{
+                    if( 54 < marks  && marks < 65){
+                        System.out.println("C");
+                    }
+                    else{
+                            System.out.println("Fail");
+                        }
+                    }
+                }
+            }
+        }
+
+        
+
+
+
+
+
+
+    }
