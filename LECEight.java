f8cb50a28b209e61b335ea67f4eaf08b23f46875
Kalindu <0101.Programming@gmail.com>
2024-03-21 21:33:26 +0530

    update #535

---
+++ LECEight.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class LECEight{
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int marks = input.nextInt();
+
+        if (marks > 74){
+            System.out.println("A"); //75-100
+        }
+        else{
+
+            if( marks > 64){
+                System.out.println("B"); //65-74
+            }
+            else{
+
+                if( marks > 54){
+                    System.out.println("C"); //64-55
+                }
+               else{
+                      System.out.println("Fail");  //54-0
+                   }
+              }
+        }
+ }
+}
