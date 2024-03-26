bb2f2cc3d316f03c98dd37430e8f1ed0e486c2e7
Kalindu <0101.Programming@gmail.com>
2024-03-26 21:20:42 +0530

    added lec9.3 and lec9.4

---
+++ lec9$3$0.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class lec9$3$0 {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 12;
+        {
+            int z = 20;
+            // System.out.println(x);
+            // System.out.println(y);
+            // System.out.println(z);
+        }
+        int z = 20;
+        int x = 30; // error
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
---
+++ lec9$4$0.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class lec9$4$0 {
+    public static void main(String args[]) {
+        final int x = 10;
+        int y = 12;
+        int z = 20;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        x = 15; // error
+        y = 25;
+        z = 30;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
