ca3f8a3765f8199605d274c35168cad25bc9f5b9
Kalindu <0101.Programming@gmail.com>
2024-03-26 19:55:56 +0530

    lec9.1.2

--- Workspace.java
+++ Workspace.java
@@ -7,6 +7,7 @@ class Workspace{
         int n1 = input.nextInt(100);
         int n2 = input.nextInt(100);
         int n3 = input.nextInt(100);
+
         System.out.println( "First  random number is " +n1 );
         System.out.println( "Second random number is " +n2 );
         System.out.println( "Third  random number is " +n3 );
@@ -16,13 +17,10 @@ class Workspace{
         }
         else if (n2>n1 && n2>n3){
             System.out.println( "Lagest number is " +n2 );
-
         }
         else {
             System.out.println("Lagest number is " + n3);
         }
-
     }
-    
-
  }
+
