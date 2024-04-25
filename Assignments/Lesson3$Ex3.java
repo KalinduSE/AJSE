e5c73fe132bc2362992846bdba0556c41069ed09
Kalindu <0101.Programming@gmail.com>
2024-04-25 19:46:10 +0530

    added lesson 3 ex 3

---
+++ Assignments/Lesson3$Ex3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Lesson3$Ex3 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int odd = number % 2;
+        if (1 < number && number < 100) {
+
+            if (odd == 1)
+                System.out.println("Weird");
+            else if (2 <= number && number <= 5)
+                System.out.println("Not Weird");
+            else if (6 <= number && number <= 20)
+                System.out.println("Weird");
+            else
+                System.out.println("Not Weird");
+                
+        }
+    }
+}
--- Workspace.java
+++ Workspace.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
-class Workspace{
-    public static void main(String[] args){
+class Workspace {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Number 1 ");
+        int number = input.nextInt();
+        math(number);
+    }
 
-      
+    public static void math(int num1) {
+        if (num1 % 3 == 0)
+            System.out.println("number can divided by 3");
+        else
+            System.out.println("number can not divided by 3");
 
-    }   
+    }
 }
