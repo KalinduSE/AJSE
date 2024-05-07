cef7531d91374dab1db1e3d3379e24d2cc6f34d5
Kalindu <0101.Programming@gmail.com>
2024-05-07 20:25:46 +0530

    -

---
+++ Lecture 17/Lecture17.1/Main.java
@@ -0,0 +1,38 @@
+public class Main{
+
+    public static void main(String[] args) {
+
+        int x= 5;
+
+        Employee emp1 = new Employee();
+        emp1.age = 14;
+        emp1.name = "Kalindu";
+        emp1.number = "0701231234";
+        System.out.println(emp1.age);
+        emp1.work();
+        emp1.talk();
+        Employee emp2 = new Employee();
+    }
+}
+
+class Employee{
+
+    String name;
+    int age;
+    String number;
+    float height;
+
+    void work(){
+
+        System.out.println("Started Working");
+
+    }
+
+    void talk(){
+
+        System.out.println("Started Talking");
+
+    }
+
+
+}
---
+++ Lecture 17/untitled/Main.java
@@ -0,0 +1,30 @@
+public class Main{
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+    }
+}
+
+class Employee{
+
+    String name;
+    int age;
+    String number;
+    float height;
+
+    void work(){
+
+        System.out.println("Started Working");
+
+    }
+
+    void talk(){
+
+        System.out.println("Started Talking");
+
+    }
+
+
+}
